00c482cae763a475384696207d1a7eace601157b
oreqizer <oreqizer@gmail.com>
2016-09-18 17:02:31 +0200

    Haskell: Fix

--- haskell/naive1.hs
+++ haskell/naive1.hs
@@ -16,7 +16,7 @@ mulV (Vector x1 y1 z1) (Vector x2 y2 z2) = Vector (x1 * x2) (y1 * y2) (z1 * z2)
 
 distV :: Vector -> Vector -> Float
 distV v1 v2 = sqrt $ x^2 + y^2 + z^2
-    where x y z = v1 `subV` v2
+    where (Vector x y z) = v1 `subV` v2
 
 -- Block
 -- ---
