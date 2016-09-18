a1c602340c374ca39630a2831e0d5b9e297de863
oreqizer <oreqizer@gmail.com>
2016-09-18 12:19:24 +0200

    Go: fmt and stuff

--- go/naive1.go
+++ go/naive1.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"math"
 	"fmt"
+	"math"
 	"time"
 )
 
@@ -27,93 +27,89 @@ func (a Vector) Mul(b Vector) Vector {
 
 func (a Vector) Dist(b Vector) float64 {
 	s := a.Sub(b)
-	return math.Sqrt(s.x * s.x + s.y * s.y + s.z * s.z)
-}
-
-func newVector(x, y, z int) Vector {
-	return Vector{float64(x), float64(y), float64(z)}
+	return math.Sqrt(s.x*s.x + s.y*s.y + s.z*s.z)
 }
 
 // Block
 // ---
 
 type Block struct {
-	loc Vector // x, y, z within a chunk
-	name string
+	loc        Vector // x, y, z within a chunk
+	name       string
 	durability int
-	typ int
-	textureid int
-	breakable bool
-	visible bool
+	typ        int
+	textureid  int
+	breakable  bool
+	visible    bool
 }
 
 // Entity
 // ---
 
-type kind int
+type Type int
 
 const (
-	Zombie kind = iota
+	Zombie Type = iota
 	Chicken
 	Creeper
 	Enderman
 )
 
 type Entity struct {
-	loc Vector // x, y, z within a chunk
-	name string
-	typ kind
-	hp int
+	loc   Vector // x, y, z within a chunk
+	typ   Type
+	name  string
+	hp    int
 	speed Vector
 }
 
-func (e Entity) Move() {
+func (e *Entity) Move() {
 	// Complex movement AI
 	rngVector := Vector{1, 1, 1}
 	moveVector := rngVector.Mul(e.speed)
 	e.loc = e.loc.Add(moveVector)
 }
 
-func newEntity(loc Vector, typ kind) Entity {
-	switch (typ) {
+func NewEntity(loc Vector, typ Type) *Entity {
+	switch typ {
 	case Zombie:
-		return Entity{
-			loc: loc,
-			name: "Zombie",
-			typ: Zombie,
-			hp: 50,
+		return &Entity{
+			loc:   loc,
+			typ:   Zombie,
+			name:  "Zombie",
+			hp:    50,
 			speed: Vector{0.5, 0, 0.5}, // slow, can't fly
 		}
 
 	case Chicken:
-		return Entity{
-			loc: loc,
-			name: "Chicken",
-			typ: Chicken,
-			hp: 25,
+		return &Entity{
+			loc:   loc,
+			typ:   Chicken,
+			name:  "Chicken",
+			hp:    25,
 			speed: Vector{0.75, 0.25, 0.75}, // can fly a bit
 		}
 
 	case Creeper:
-		return Entity{
-			loc: loc,
-			name: "Creeper",
-			typ: Creeper,
-			hp: 75,
+		return &Entity{
+			loc:   loc,
+			typ:   Creeper,
+			name:  "Creeper",
+			hp:    75,
 			speed: Vector{0.75, 0, 0.75},
 		}
 
 	case Enderman:
-		return Entity{
-			loc: loc,
-			name: "Enderman",
-			typ: Enderman,
-			hp: 500,
+		return &Entity{
+			loc:   loc,
+			typ:   Enderman,
+			name:  "Enderman",
+			hp:    500,
 			speed: Vector{1, 1, 1}, // does what he wants
 		}
 
 	default:
-		return Entity{}
+		return &Entity{}
 	}
 }
 
@@ -121,14 +117,14 @@ func newEntity(loc Vector, typ kind) Entity {
 // ---
 
 const (
-	ChunkBlocks = 65536
+	ChunkBlocks   = 65536
 	ChunkEntities = 1000
 )
 
 type Chunk struct {
-	loc Vector // x, y, z within the world
-	blocks []Block
-	entities []Entity
+	loc      Vector // x, y, z within the world
+	blocks   []Block
+	entities []*Entity
 }
 
 func (c *Chunk) ProcessEntities() {
@@ -141,27 +137,29 @@ func genBlocks() []Block {
 	blocks := make([]Block, ChunkBlocks)
 	for i := 0; i < ChunkBlocks; i++ {
 		name := fmt.Sprintf("Block:%d", i)
-		blocks = append(blocks, Block{newVector(i, i, i), name, 100, 1, 1, true, true})
+		f := float64(i)
+		blocks = append(blocks, Block{Vector{f, f, f}, name, 100, 1, 1, true, true})
 	}
 	return blocks
 }
 
-func genEntities() []Entity {
-	entities := make([]Entity, ChunkEntities)
-	for i := 0; i < ChunkEntities / 4; i++ {
+func genEntities() []*Entity {
+	entities := make([]*Entity, ChunkEntities)
+	for i := 0; i < ChunkEntities/4; i++ {
 		// Fancy procedural generation initial position equation
-		entities = append(entities, newEntity(newVector(i, i, i), Zombie))
-		entities = append(entities, newEntity(newVector(i + 1, i, i), Chicken))
-		entities = append(entities, newEntity(newVector(i + 2, i, i), Creeper))
-		entities = append(entities, newEntity(newVector(i + 3, i, i), Enderman))
+		f := float64(i)
+		entities = append(entities, NewEntity(Vector{f, f, f}, Zombie))
+		entities = append(entities, NewEntity(Vector{f + 1, f, f}, Chicken))
+		entities = append(entities, NewEntity(Vector{f + 2, f, f}, Creeper))
+		entities = append(entities, NewEntity(Vector{f + 3, f, f}, Enderman))
 	}
 	return entities
 }
 
-func newChunk(loc Vector) *Chunk {
+func NewChunk(loc Vector) *Chunk {
 	return &Chunk{
-		loc: loc,
-		blocks: genBlocks(),
+		loc:      loc,
+		blocks:   genBlocks(),
 		entities: genEntities(),
 	}
 }
@@ -174,14 +172,14 @@ const (
 )
 
 type Game struct {
-	chunks []*Chunk
+	chunks     []*Chunk
 	chunkCount int
-	playerLoc Vector
+	playerLoc  Vector
 }
 
 func (g *Game) LoadWorld() {
 	for i := 0; i < GameChunks; i++ {
-		g.chunks = append(g.chunks, newChunk(newVector(g.chunkCount, 0, 0)))
+		g.chunks = append(g.chunks, NewChunk(Vector{float64(g.chunkCount), 0, 0}))
 		g.chunkCount++
 	}
 }
@@ -199,17 +197,16 @@ func (g *Game) UpdateChunks() {
 
 	for _, id := range rmChunks {
 		// see https://github.com/golang/go/wiki/SliceTricks
-		copy(g.chunks[:id], g.chunks[id + 1:])
-		g.chunks[len(g.chunks) - 1] = newChunk(newVector(g.chunkCount, 0, 0))
+		copy(g.chunks[:id], g.chunks[id+1:])
+		g.chunks[len(g.chunks)-1] = NewChunk(Vector{float64(g.chunkCount), 0, 0})
 		g.chunkCount++
 	}
 }
 
 func newGame() *Game {
 	return &Game{
-		chunks: make([]*Chunk, GameChunks),
 		chunkCount: 0,
-		playerLoc: Vector{0, 0, 0},
+		playerLoc:  Vector{0, 0, 0},
 	}
 }
 
@@ -217,7 +214,7 @@ func newGame() *Game {
 // ---
 
 func main() {
-	game := &Game{}
+	game := newGame()
 	fmt.Println("Loading World...")
 	start := time.Now()
 	game.LoadWorld()
