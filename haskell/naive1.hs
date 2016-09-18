0a1f9f69af6cb306c51894fb2fd0f957ef06c532
oreqizer <oreqizer@gmail.com>
2016-09-18 17:01:50 +0200

    Haskell: Init

---
+++ haskell/naive1.hs
@@ -0,0 +1,35 @@
+module Main where
+
+-- Vector
+-- ---
+
+data Vector = Vector Float Float Float
+
+addV :: Vector -> Vector -> Vector
+addV (Vector x1 y1 z1) (Vector x2 y2 z2) = Vector (x1 + x2) (y1 + y2) (z1 + z2)
+
+subV :: Vector -> Vector -> Vector
+subV (Vector x1 y1 z1) (Vector x2 y2 z2) = Vector (x1 - x2) (y1 - y2) (z1 - z2)
+
+mulV :: Vector -> Vector -> Vector
+mulV (Vector x1 y1 z1) (Vector x2 y2 z2) = Vector (x1 * x2) (y1 * y2) (z1 * z2)
+
+distV :: Vector -> Vector -> Float
+distV v1 v2 = sqrt $ x^2 + y^2 + z^2
+    where x y z = v1 `subV` v2
+
+-- Block
+-- ---
+
+blockTypes :: Int
+blockTypes = 256
+
+data Block = Block { bLocation :: Vector
+                   , bName :: Text -- TODO
+                   } deriving (Show)
+
+-- main
+-- ---
+
+main :: IO ()
+main = putStr "temp"
