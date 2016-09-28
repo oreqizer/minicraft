c14744f8d5d0d1ff6092b25417815ece1be66485
oreqizer <oreqizer@gmail.com>
2016-09-28 10:29:39 +0200

    C: Init

---
+++ c/naive1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+// vector
+// ---
+
+struct Vector {
+    float x;
+    float y;
+    float z;
+};
+
+//Vector addV(a Vector, b Vector) {
+//    return Vector{a.x + b.x, a.y + b.y, a.z + b.z};
+//}
+//
+//Vector subV(a Vector, b Vector) {
+//    return Vector{a.x - b.x, a.y - b.y, a.z - b.z};
+//}
+//
+//Vector mulV(a Vector, b Vector) {
+//    return Vector{a.x * b.x, a.y * b.y, a.z * b.z};
+//}
+//
+//Vector distV(a Vector, b Vector) {
+//    v = subV(a, b);
+//    return sqrt(v.x*v.x + v.y*v.y + v.z*v.z);
+//}
+
+// main
+// ---
+
+int main(int argc, char *argv[]) {
+
+}
